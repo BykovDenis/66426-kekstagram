d01ae6d7a9ff1d1173ce8319592939f611c2df32
Bykov Denis <stivenrain@mail.com>
2016-10-07 12:09:41 +0300

    #6 Начинаем программировать

--- src/js/check.js
+++ src/js/check.js
@@ -1,7 +1,7 @@
 var getMessage = function(a, b){
 	if(typeof a === "boolean"){
 		if(a){
-			return b;
+			return  "Переданное GIF-изображение анимировано и содержит "+b+" кадров";
 		}
 		else{
 			return "Переданное GIF-изображение не анимировано";
@@ -10,5 +10,23 @@ var getMessage = function(a, b){
 	if(typeof a === "number"){
 		return "Переданное SVG-изображение содержит "+a+" объектов и "+(b * 4)+" атрибутов"
 	}
+	if(typeof a === "object"){
+
+		if(typeof b === "object"){
+			var artifactsSquare = 0;
+			for(var i = 0, len = a.length; i < len; i++){
+				artifactsSquare += a[i] * b[i];
+			}
+			return "Общая площадь артефактов сжатия: "+artifactsSquare+" пикселей";
+		}
+
+		var amountOfRedPoints = 0;
+		for(var i=0,len=a.length;i < len;i++){
+			amountOfRedPoints += a[i];
+		}
+		return "Количество красных точек во всех строчках изображения: "+amountOfRedPoints;
+	}
+
+	return "Переданы некорректные данные";   
 
 }
