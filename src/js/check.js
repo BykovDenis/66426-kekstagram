569acec252b4da799b302cc3932f43781b3fa844
Denis Bykov <dbykov@owm.io>
2016-10-10 14:21:02 +0300

    #8 Canvas внесены правки по требованиям eslinter

--- src/js/check.js
+++ src/js/check.js
@@ -1,27 +1,27 @@
 'use strict';
 
-var getMessage = function(a, b) {
-  if(typeof a === 'boolean') {
-    if(a) {
+window.getMessage = function(a, b) {
+  var i, len;
+  if (typeof a === 'boolean') {
+    if (a) {
       return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
-    }
-    else {
+    } else {
       return 'Переданное GIF-изображение не анимировано';
     }
   }
-  if(typeof a === 'number') {
+  if (typeof a === 'number') {
     return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b * 4) + ' атрибутов';
   }
-  if(typeof a === 'object') {
-    if(typeof b === 'object') {
+  if (typeof a === 'object') {
+    if (typeof b === 'object') {
       var artifactsSquare = 0;
-      for(var i = 0, len = a.length; i < len; i++ ) {
+      for (i = 0, len = a.length; i < len; i++ ) {
         artifactsSquare += a[i] * b[i];
       }
       return 'Общая площадь артефактов сжатия: ' + artifactsSquare + ' пикселей';
     }
     var amountOfRedPoints = 0;
-    for(var i = 0, len = a.length; i < len; i++) {
+    for (i = 0, len = a.length; i < len; i++) {
       amountOfRedPoints += a[i];
     }
     return 'Количество красных точек во всех строчках изображения: ' + amountOfRedPoints;
