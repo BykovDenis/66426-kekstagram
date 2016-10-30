d929b8eaca234bfaca5a5ae3ad715f3e88a5bbd4
Bykov Denis <stivenrain@mail.com>
2016-10-30 23:48:15 +0300

    #20 Второй класс

--- src/picture.js
+++ src/picture.js
@@ -3,15 +3,58 @@
 
 var gallery = require('./gallery');
 
-module.exports = function(img, index, pictureCard, imgSide) {
-  img.width = imgSide;
-  img.height = imgSide;
-  var currentImg = pictureCard.querySelector('img');
-  pictureCard.replaceChild(img, currentImg);
+var Picture = function(card, index, pictureCard) {
 
-  img.onclick = function(event) {
+  // 10 время на загрузку картинки
+  var IMAGE_LOAD_TIMEOUT = 10000;
+  var IMG_SIDE = 182;
+
+  this.data = {
+    pictureCard: pictureCard,
+    card: card,
+    index: index,
+  };
+
+  // Создаем картинку
+  this.data.img = new Image();
+  var that = this;
+
+  this.data.img.onload = function() {
+    clearTimeout(timeOutLoading);
+    var currentImg = that.data.pictureCard.querySelector('img');
+    pictureCard.replaceChild(this, currentImg);
+
+    that.remove();
+  };
+
+  var error = function() {
+    pictureCard.classList.add('picture-load-failure');
+  };
+
+  var onclick = function(event) {
     event.preventDefault();
     gallery.show(index);
   };
 
+  this.data.img.onerror = error;
+
+  this.data.img.width = IMG_SIDE;
+  this.data.img.height = IMG_SIDE;
+  this.data.img.src = this.data.card.url;
+
+  this.data.img.onclick = onclick;
+
+  var timeOutLoading = setTimeout(error, IMAGE_LOAD_TIMEOUT);
+
+  this.element = pictureCard;
+
 };
+
+Picture.prototype = {
+  remove: function() {
+    this.data.img.onload = null;
+    this.data.img.error = null;
+  },
+};
+
+module.exports = Picture;
